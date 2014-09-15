485724608551b1e19c85fe7b82a523c8c4d1226a
Sirone <crepuscular.eva@gmail.com>
2014-09-15 13:25:35 +0900

    引数がラジアンになっていなかったので修正. style は全て CSS 化. textarea タグで JSON 配列を吐き出した(これが本来の目的).

--- app/webroot/index.php
+++ app/webroot/index.php
@@ -2,9 +2,13 @@
   <head>
     <title>正弦・余弦・正接テーブル</title>
     <?php $version = time(); ?>
+    <?php $data = array( 'sin' => array(), 'cos'=>array(), 'tan'=>array() ); ?>
     <link rel="stylesheet" href="css/default.css?v=<?php echo $version; ?>">
   </head>
   <body>
+    <?php // _/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/ ?>
+    <?php // ページ上部メニュー ?>
+    <?php // _/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/ ?>
     <div class="menu" id="menu">
       <div class="menu-title">
         コンテンツメニュー
@@ -17,8 +21,14 @@
       </div>
     </div>
 
-    <h1 id="all-table">正弦・余弦・正接</h1>
-    <table border="1" style="border-spacing:0;">
+    <?php // _/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/ ?>
+    <?php // 正弦・余弦・正接 ?>
+    <?php // _/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/ ?>
+    <div>
+      <h1 id="all-table">正弦・余弦・正接</h1>
+      <a href="#menu">メニューに戻る</a>
+    </div>
+    <table>
       <thead>
         <tr>
           <td>度</td>
@@ -31,15 +41,27 @@
 <?php for( $i=0; $i<=180; $i++ ) : ?>
         <tr>
           <td><?php echo sprintf('%d°',$i); ?></td>
-          <td><pre><?php echo sprintf("%' 9.6f",sin($i)); ?></pre></td>
-          <td><pre><?php echo sprintf("%' 9.6f",cos($i)); ?></pre></td>
-          <td><pre><?php echo sprintf("%' 9.4f",tan($i)); ?></pre></td>
+          <td><pre><?php echo $data['sin'][$i]=sprintf("%' 9.6f",sin(deg2rad($i))); ?></pre></td>
+          <td><pre><?php echo $data['cos'][$i]=sprintf("%' 9.6f",cos(deg2rad($i))); ?></pre></td>
+          <td><pre><?php echo $data['tan'][$i]=sprintf("%' 9.4f",tan(deg2rad($i))); ?></pre></td>
         </tr>
+        <?php // JSON 用に型キャスト. ?>
+        <?php $data['sin'][$i] = (float)$data['sin'][$i]; ?>
+        <?php $data['cos'][$i] = (float)$data['cos'][$i]; ?>
+        <?php $data['tan'][$i] = (float)$data['tan'][$i]; ?>
 <?php endfor; ?>
       </tbody>
     </table>
-    <h1 id="sin-table">正弦のみ</h1><a href="#menu">メニューに戻る</a>
-    <table border="1" style="border-spacing:0;">
+    <br class="clearfix" />
+
+    <?php // _/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/ ?>
+    <?php // 正弦 ?>
+    <?php // _/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/ ?>
+    <div>
+      <h1 id="sin-table">正弦</h1>
+      <a href="#menu">メニューに戻る</a>
+    </div>
+    <table>
       <thead>
         <tr>
           <td>正弦</td>
@@ -48,13 +70,25 @@
       <tbody>
 <?php for( $i=0; $i<=180; $i++ ) : ?>
         <tr>
-          <td><pre><?php echo sprintf("%' 9.6f",sin($i)); ?></pre></td>
+          <td><pre><?php echo sprintf("%' 9.6f",sin(deg2rad($i))); ?></pre></td>
         </tr>
 <?php endfor; ?>
       </tbody>
     </table>
-    <h1 id="cos-table">余弦のみ</h1><a href="#menu">メニューに戻る</a>
-    <table border="1" style="border-spacing:0;">
+    <?php // _/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/ ?>
+    <?php // 正弦 JSON 配列 ?>
+    <?php // _/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/ ?>
+    <textarea cols="55" rows="30"><?php echo json_encode( $data['sin'] ); ?></textarea>
+    <br class="clearfix" />
+
+    <?php // _/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/ ?>
+    <?php // 余弦 ?>
+    <?php // _/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/ ?>
+    <div>
+      <h1 id="cos-table">余弦</h1>
+      <a href="#menu">メニューに戻る</a>
+    </div>
+    <table>
       <thead>
         <tr>
           <td>余弦</td>
@@ -63,13 +97,25 @@
       <tbody>
 <?php for( $i=0; $i<=180; $i++ ) : ?>
         <tr>
-          <td><pre><?php echo sprintf("%' 9.6f",cos($i)); ?></pre></td>
+          <td><pre><?php echo sprintf("%' 9.6f",cos(deg2rad($i))); ?></pre></td>
         </tr>
 <?php endfor; ?>
       </tbody>
     </table>
-    <h1 id="tan-table">正接のみ</h1><a href="#menu">メニューに戻る</a>
-    <table border="1" style="border-spacing:0;">
+    <?php // _/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/ ?>
+    <?php // 余弦 JSON 配列 ?>
+    <?php // _/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/ ?>
+    <textarea cols="55" rows="30"><?php echo json_encode( $data['cos'] ); ?></textarea>
+    <br class="clearfix" />
+
+    <?php // _/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/ ?>
+    <?php // 正接 ?>
+    <?php // _/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/ ?>
+    <div>
+      <h1 id="tan-table">正接</h1>
+      <a href="#menu">メニューに戻る</a>
+    </div>
+    <table>
       <thead>
         <tr>
           <td>正接</td>
@@ -78,10 +124,15 @@
       <tbody>
 <?php for( $i=0; $i<=180; $i++ ) : ?>
         <tr>
-          <td><pre><?php echo sprintf("%' 9.4f",tan($i)); ?></pre></td>
+          <td><pre><?php echo sprintf("%' 9.4f",tan(deg2rad($i))); ?></pre></td>
         </tr>
 <?php endfor; ?>
       </tbody>
     </table>
+    <?php // _/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/ ?>
+    <?php // 正接 JSON 配列 ?>
+    <?php // _/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/ ?>
+    <textarea cols="55" rows="30"><?php echo json_encode( $data['tan'] ); ?></textarea>
+    <br class="clearfix" />
   </body>
 </html>
